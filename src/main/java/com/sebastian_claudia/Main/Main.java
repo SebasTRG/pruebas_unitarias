bb475d6c8aeb5f27d519e8f71363158a991b295d
Sebastian Rengifo <sebastrg16@gmail.com>
2024-04-13 17:30:45 +0200

    Estructura agregaada

---
+++ src/main/java/com/sebastian_claudia/Main/Main.java
@@ -0,0 +1,13 @@
+package com.sebastian_claudia.Main;
+
+/**
+ *
+ * @author Sebastian Rengifo
+ * @author Claudia Durán
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
